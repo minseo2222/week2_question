5b388c04a8c6dc1fd7b43b00854824e3548f4a5c
minseo2222 <144326203+minseo2222@users.noreply.github.com>
2024-03-12 12:50:10 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -1,45 +1,45 @@
 class PriorityQueue:
-    def __init__(self):
-        self.queue = []
+  def __init__(self):
+    self.queue = []
 
-    def push(self, item, priority):
-        self.queue.append((priority, item))
-        self.queue.sort(reverse=True)  # 우선순위가 높은 순서로 정렬
+  def push(self, item, priority):
+    self.queue.append((priority, item))
+    self.queue.sort(reverse=True)  # 우선순위가 높은 순서로 정렬
 
-    def pop(self):
-        if self.queue:
-            return self.queue.pop(0)[1]  # 가장 우선순위가 높은 요소 반환
-        else:
-            return None  # 큐가 비어있을 경우 None 반환
+  def pop(self):
+    if self.queue:
+      return self.queue.pop(0)[1]  # 가장 우선순위가 높은 요소 반환
+    else:
+      return None  # 큐가 비어있을 경우 None 반환
 
-    def _sift_up(self, index):
-        while index > 0:
-            parent_index = (index - 1) // 2
-            if self.heap[parent_index][0] > self.heap[index][0]:
-                self._swap(parent_index, index)
-                index = parent_index
-            else:
-                break
+  def _sift_up(self, index):
+    while index > 0:
+      parent_index = (index - 1) // 2
+      if self.heap[parent_index][0] > self.heap[index][0]:
+        self._swap(parent_index, index)
+        index = parent_index
+      else:
+        break
 
-    def _sift_down(self, index):
-        while True:
-            left_child_index = 2 * index + 1
-            right_child_index = 2 * index + 2
-            smallest = index
+  def _sift_down(self, index):
+    while True:
+      left_child_index = 2 * index + 1
+      right_child_index = 2 * index + 2
+      smallest = index
 
-            if left_child_index < len(self.heap) and \
-                    self.heap[left_child_index][0] < self.heap[smallest][0]:
-                smallest = left_child_index
+      if left_child_index < len(self.heap) and \
+          self.heap[left_child_index][0] > self.heap[smallest][0]:
+        smallest = left_child_index
 
-            if right_child_index < len(self.heap) and \
-                    self.heap[right_child_index][0] < self.heap[smallest][0]:
-                smallest = right_child_index
+      if right_child_index < len(self.heap) and \
+          self.heap[right_child_index][0] > self.heap[smallest][0]:
+        smallest = right_child_index
 
-            if smallest != index:
-                self._swap(index, smallest)
-                index = smallest
-            else:
-                break
+      if smallest != index:
+        self._swap(index, smallest)
+        index = smallest
+      else:
+        break
 
-    def _swap(self, i, j):
-        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+  def _swap(self, i, j):
+    self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
