b5071fec7ada58dad2c56971cb55bbb0238f8e21
minseo2222 <144326203+minseo2222@users.noreply.github.com>
2024-03-12 12:47:02 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -12,34 +12,34 @@ class PriorityQueue:
         else:
             return None  # 큐가 비어있을 경우 None 반환
 
-  def _sift_up(self, index):
-    while index > 0:
-      parent_index = (index - 1) // 2
-      if self.heap[parent_index][0] > self.heap[index][0]:
-        self._swap(parent_index, index)
-        index = parent_index
-      else:
-        break
+    def _sift_up(self, index):
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.heap[parent_index][0] > self.heap[index][0]:
+                self._swap(parent_index, index)
+                index = parent_index
+            else:
+                break
 
-  def _sift_down(self, index):
-    while True:
-      left_child_index = 2 * index + 1
-      right_child_index = 2 * index + 2
-      smallest = index
+    def _sift_down(self, index):
+        while True:
+            left_child_index = 2 * index + 1
+            right_child_index = 2 * index + 2
+            smallest = index
 
-      if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][0] > self.heap[smallest][0]:
-        smallest = left_child_index
+            if left_child_index < len(self.heap) and \
+                    self.heap[left_child_index][0] > self.heap[smallest][0]:
+                smallest = left_child_index
 
-      if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][0] > self.heap[smallest][0]:
-        smallest = right_child_index
+            if right_child_index < len(self.heap) and \
+                    self.heap[right_child_index][0] > self.heap[smallest][0]:
+                smallest = right_child_index
 
-      if smallest != index:
-        self._swap(index, smallest)
-        index = smallest
-      else:
-        break
+            if smallest != index:
+                self._swap(index, smallest)
+                index = smallest
+            else:
+                break
 
-  def _swap(self, i, j):
-    self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+    def _swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
